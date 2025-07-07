cd05a97e6e566839918569404ab971f40ba8fe32
ch532 <trustconnect713@gmail.com>
2025-07-07 04:00:46 +0100

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set DIR=%~dp0
-set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-if not exist "%JAVA_EXE%" set JAVA_EXE=java
-"%JAVA_EXE%" -cp "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
