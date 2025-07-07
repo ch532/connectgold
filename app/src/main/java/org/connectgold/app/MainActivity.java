49e62bbb54f24fc30a32b1a912848dfa0eb03f93
ch532 <trustconnect713@gmail.com>
2025-07-07 02:46:56 +0100

    Update MainActivity.java

--- app/src/main/java/org/connectgold/app/MainActivity.java
+++ app/src/main/java/org/connectgold/app/MainActivity.java
@@ -1,61 +1,66 @@
-// MainActivity.java package com.connectgold.app;
+package com.connectgold.app;
 
-import android.os.Bundle; import android.webkit.WebView; import android.webkit.WebViewClient; import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.ads.AdRequest; import com.google.android.gms.ads.AdView; import com.google.android.gms.ads.MobileAds; import com.google.android.gms.ads.interstitial.InterstitialAd; import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback; import com.google.android.gms.ads.rewarded.RewardedAd; import com.google.android.gms.ads.rewarded.RewardItem; import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+import com.google.android.gms.ads.*;
+import com.appodeal.ads.Appodeal;
+import com.appodeal.ads.utils.LogLevel;
 
-import com.appodeal.ads.Appodeal; import com.appodeal.ads.AppodealNative; import com.appodeal.ads.utils.LogLevel;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity { private WebView webView; private AdView bannerAdView; private InterstitialAd mInterstitialAd; private RewardedAd rewardedAd; private static final String ADMOB_APP_ID = "ca-app-pub-1171216593802007~4173705766"; private static final String BANNER_AD_UNIT_ID = "ca-app-pub-1171216593802007/8884489855"; private static final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-1171216593802007/4945244849"; private static final String REWARDED_AD_UNIT_ID = "ca-app-pub-1171216593802007/1435861786"; private static final String NATIVE_AD_UNIT_ID = "ca-app-pub-1171216593802007/5226917075"; private static final String APPODEAL_KEY = "923bc30ebaca5186de21bbebb9612173a8759ba61bbb4ed0";
+    private WebView webView;
+    private AdView adView;
+    private InterstitialAd interstitialAd;
+    private RewardedAd rewardedAd;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    private final String ADMOB_APP_ID = "ca-app-pub-1171216593802007~4173705766";
+    private final String BANNER_AD_UNIT_ID = "ca-app-pub-1171216593802007/8884489855";
+    private final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-1171216593802007/4945244849";
+    private final String REWARDED_AD_UNIT_ID = "ca-app-pub-1171216593802007/1435861786";
+    private final String APPODEAL_KEY = "923bc30ebaca5186de21bbebb9612173a8759ba61bbb4ed0";
 
-    // Load WebView
-    webView = findViewById(R.id.webview);
-    webView.setWebViewClient(new WebViewClient());
-    webView.getSettings().setJavaScriptEnabled(true);
-    webView.loadUrl("https://connectgold.sbs");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    // Initialize AdMob
-    MobileAds.initialize(this, initializationStatus -> {});
+        // WebView setup
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://connectgold.sbs");
 
-    // Load Banner Ad
-    bannerAdView = findViewById(R.id.adView);
-    AdRequest adRequest = new AdRequest.Builder().build();
-    bannerAdView.loadAd(adRequest);
+        // AdMob init
+        MobileAds.initialize(this, initializationStatus -> {});
 
-    // Load Interstitial Ad
-    InterstitialAd.load(this, INTERSTITIAL_AD_UNIT_ID, adRequest, new InterstitialAdLoadCallback() {
-        @Override
-        public void onAdLoaded(InterstitialAd ad) {
-            mInterstitialAd = ad;
-            mInterstitialAd.show(MainActivity.this);
-        }
-    });
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
-    // Load Rewarded Ad
-    RewardedAd.load(this, REWARDED_AD_UNIT_ID, adRequest, new RewardedAdLoadCallback() {
-        @Override
-        public void onAdLoaded(RewardedAd ad) {
-            rewardedAd = ad;
-            rewardedAd.show(MainActivity.this, rewardItem -> {
-                int rewardAmount = rewardItem.getAmount();
-                String rewardType = rewardItem.getType();
-                // Handle reward logic here
-            });
-        }
-    });
+        InterstitialAd.load(this, INTERSTITIAL_AD_UNIT_ID, adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(InterstitialAd ad) {
+                interstitialAd = ad;
+                interstitialAd.show(MainActivity.this);
+            }
+        });
 
-    // Initialize Appodeal
-    Appodeal.setLogLevel(LogLevel.DEBUG);
-    Appodeal.initialize(this, APPODEAL_KEY, 
-        Appodeal.BANNER | Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO | Appodeal.NATIVE);
+        RewardedAd.load(this, REWARDED_AD_UNIT_ID, adRequest, new RewardedAdLoadCallback() {
+            @Override
+            public void onAdLoaded(RewardedAd ad) {
+                rewardedAd = ad;
+                rewardedAd.show(MainActivity.this, reward -> {
+                    // reward logic
+                });
+            }
+        });
 
-    Appodeal.show(this, Appodeal.BANNER_BOTTOM);
+        // Appodeal init
+        Appodeal.setLogLevel(LogLevel.DEBUG);
+        Appodeal.initialize(this, APPODEAL_KEY, Appodeal.INTERSTITIAL | Appodeal.BANNER | Appodeal.REWARDED_VIDEO | Appodeal.NATIVE);
+        Appodeal.show(this, Appodeal.BANNER_BOTTOM);
+    }
 }
-
-}
-
