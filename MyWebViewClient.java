8531ea5bfbd4975978d3a0fc1248f3ea5850ceb2
ch532 <trustconnect713@gmail.com>
2025-07-07 14:33:12 +0100

    Update MyWebViewClient.java

--- MyWebViewClient.java
+++ MyWebViewClient.java
@@ -1,25 +1,38 @@
-package com.example.app;
+package com.connectgold.app;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.util.Log;
 
-class MyWebViewClient extends WebViewClient {
+public class MyWebViewClient extends WebViewClient {
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        String hostname;
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        String clickedUrl = request.getUrl().toString();
+        Log.d("WebView", "Clicked URL: " + clickedUrl);
 
-        // YOUR HOSTNAME
-        hostname = "example.com";
-
-        Uri uri = Uri.parse(url);
-        if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
-            return false;
+        // Add your tracking logic here
+        if (clickedUrl.contains("utm_source=facebook")) {
+            Log.d("Analytics", "Facebook traffic detected");
         }
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        view.getContext().startActivity(intent);
+
+        view.loadUrl(clickedUrl);
         return true;
+    }
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        Log.d("WebView", "Loading: " + url);
+        // You can show a loader here
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        Log.d("WebView", "Finished: " + url);
+        // You can hide the loader here
     }
 }
