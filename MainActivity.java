4588aa056ee095145a119440efbc5dbfba8fc33a
ch532 <trustconnect713@gmail.com>
2025-07-07 14:17:13 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,127 @@
-package com.example.app;
+package com.connectgold.app;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.ads.*;
+import com.google.android.gms.ads.nativead.*;
 
-    private WebView mWebView;
+import com.appodeal.ads.Appodeal;
+import com.appodeal.ads.BannerCallbacks;
+import com.appodeal.ads.InterstitialCallbacks;
+import com.appodeal.ads.RewardedVideoCallbacks;
+import com.appodeal.ads.utils.LogLevel;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private AdView bannerAdView;
+    private InterstitialAd interstitialAd;
+    private RewardedAd rewardedAd;
+
+    private final String ADMOB_APP_ID = "ca-app-pub-1171216593802007~4173705766";
+    private final String BANNER_AD_UNIT_ID = "ca-app-pub-1171216593802007/8884489855";
+    private final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-1171216593802007/4945244849";
+    private final String REWARDED_AD_UNIT_ID = "ca-app-pub-1171216593802007/1435861786";
+    private final String NATIVE_AD_UNIT_ID = "ca-app-pub-1171216593802007/5226917075";
+    private final String APPODEAL_KEY = "923bc30ebaca5186de21bbebb9612173a8759ba61bbb4ed0";
 
     @Override
-    @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWebView = findViewById(R.id.activity_main_webview);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new MyWebViewClient());
+        
+        // Load website
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://connectgold.sbs");
 
-        // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+        // Init AdMob
+        MobileAds.initialize(this, initializationStatus -> {});
 
-        // LOCAL RESOURCE
-        // mWebView.loadUrl("file:///android_asset/index.html");
+        // Show banner ad
+        bannerAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        bannerAdView.loadAd(adRequest);
+
+        // Show interstitial ad
+        InterstitialAd.load(this, INTERSTITIAL_AD_UNIT_ID, adRequest, new InterstitialAdLoadCallback() {
+            public void onAdLoaded(InterstitialAd ad) {
+                interstitialAd = ad;
+                interstitialAd.show(MainActivity.this);
+            }
+        });
+
+        // Show rewarded ad
+        RewardedAd.load(this, REWARDED_AD_UNIT_ID, adRequest, new RewardedAdLoadCallback() {
+            public void onAdLoaded(RewardedAd ad) {
+                rewardedAd = ad;
+                rewardedAd.show(MainActivity.this, reward -> {
+                    Log.d("AdMob", "User earned reward.");
+                });
+            }
+        });
+
+        // Load native ad
+        AdLoader adLoader = new AdLoader.Builder(this, NATIVE_AD_UNIT_ID)
+                .forNativeAd(nativeAd -> {
+                    NativeAdView adView = findViewById(R.id.nativeAdView);
+                    populateNativeAdView(nativeAd, adView);
+                })
+                .build();
+        adLoader.loadAd(new AdRequest.Builder().build());
+
+        // Init Appodeal
+        Appodeal.setLogLevel(LogLevel.DEBUG);
+        Appodeal.initialize(this, APPODEAL_KEY, Appodeal.INTERSTITIAL | Appodeal.BANNER | Appodeal.REWARDED_VIDEO | Appodeal.NATIVE);
+
+        Appodeal.show(this, Appodeal.BANNER_BOTTOM);
+
+        Appodeal.setInterstitialCallbacks(new InterstitialCallbacks() {
+            public void onInterstitialClicked() { Log.d("Appodeal", "Interstitial Clicked"); }
+            public void onInterstitialShown() { Log.d("Appodeal", "Shown"); }
+            public void onInterstitialLoaded(boolean isPrecache) { Log.d("Appodeal", "Loaded"); }
+            public void onInterstitialFailedToLoad() { Log.d("Appodeal", "Failed"); }
+            public void onInterstitialClosed() { Log.d("Appodeal", "Closed"); }
+            public void onInterstitialExpired() { Log.d("Appodeal", "Expired"); }
+            public void onInterstitialShowFailed() { Log.d("Appodeal", "Show Failed"); }
+        });
+
+        Appodeal.setRewardedVideoCallbacks(new RewardedVideoCallbacks() {
+            public void onRewardedVideoClicked() { Log.d("Appodeal", "Rewarded Clicked"); }
+            public void onRewardedVideoShown() { Log.d("Appodeal", "Shown"); }
+            public void onRewardedVideoClosed(boolean finished) { Log.d("Appodeal", "Closed"); }
+            public void onRewardedVideoCompleted() { Log.d("Appodeal", "Completed"); }
+            public void onRewardedVideoFailedToLoad() { Log.d("Appodeal", "Failed to Load"); }
+            public void onRewardedVideoExpired() { Log.d("Appodeal", "Expired"); }
+            public void onRewardedVideoLoaded(boolean isPrecache) { Log.d("Appodeal", "Loaded"); }
+        });
+
+        Appodeal.setBannerCallbacks(new BannerCallbacks() {
+            public void onBannerClicked() { Log.d("Appodeal", "Banner Clicked"); }
+            public void onBannerShown() { Log.d("Appodeal", "Banner Shown"); }
+            public void onBannerFailedToLoad() { Log.d("Appodeal", "Banner Failed"); }
+            public void onBannerLoaded(int height, boolean isPrecache) { Log.d("Appodeal", "Banner Loaded"); }
+            public void onBannerExpired() { Log.d("Appodeal", "Banner Expired"); }
+        });
     }
 
-    @Override
-    public void onBackPressed() {
-        if(mWebView.canGoBack()) {
-            mWebView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    private void populateNativeAdView(NativeAd nativeAd, NativeAdView adView) {
+        TextView headline = adView.findViewById(R.id.headline);
+        TextView body = adView.findViewById(R.id.body);
+        Button callToAction = adView.findViewById(R.id.call_to_action);
+
+        headline.setText(nativeAd.getHeadline());
+        body.setText(nativeAd.getBody());
+        callToAction.setText(nativeAd.getCallToAction());
+
+        adView.setHeadlineView(headline);
+        adView.setBodyView(body);
+        adView.setCallToActionView(callToAction);
+        adView.setNativeAd(nativeAd);
     }
 }
